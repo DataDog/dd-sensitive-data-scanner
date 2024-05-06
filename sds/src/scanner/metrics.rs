382e492e80b97ab6c57f6672f5e028ab21e40ad9
Arthur Belleville <arthur.belleville@datadoghq.com>
2024-05-06 15:12:44 +0200

    Remove Default implementation + rename metric + remove extra labels

--- sds/src/scanner/metrics.rs
+++ sds/src/scanner/metrics.rs
@@ -1,25 +1,17 @@
-use crate::labels::{Labels, NO_LABEL};
+use crate::labels::{Labels};
 use metrics::{counter, Counter};
 
 pub struct Metrics {
     pub false_positive_excluded_attributes: Counter,
 }
 
-const TYPE: &str = "type";
-
 impl Metrics {
     pub fn new(labels: &Labels) -> Self {
         Metrics {
             false_positive_excluded_attributes: counter!(
-                "false_positive.excluded_attributes",
-                labels.clone_with_labels(Labels::new(&[(TYPE, "excluded_attributes".to_string())]))
+                "false_positive.multipass.excluded_match",
+                labels.clone()
             ),
         }
-    }
-}
-
-impl Default for Metrics {
-    fn default() -> Self {
-        Metrics::new(&NO_LABEL)
     }
 }
