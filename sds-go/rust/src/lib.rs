20e9e6052eb320fef761965e7dd5388d9040223e
Nathan <fuchsnj@gmail.com>
2024-05-06 08:47:31 -0400

    Block accidental use of `println` (#42)
    * block println
    
    * add to sds-binding-utils- and sds-go also

--- sds-go/rust/src/lib.rs
+++ sds-go/rust/src/lib.rs
@@ -1,1 +1,5 @@
+// This blocks accidental use of `println`. If one is actually needed, you can
+// override with `#[allow(clippy::print_stdout)]`.
+#![deny(clippy::print_stdout)]
+
 mod native;
--- sds/src/lib.rs
+++ sds/src/lib.rs
@@ -1,3 +1,7 @@
+// This blocks accidental use of `println`. If one is actually needed, you can
+// override with `#[allow(clippy::print_stdout)]`.
+#![deny(clippy::print_stdout)]
+
 mod encoding;
 mod event;
 mod match_action;
