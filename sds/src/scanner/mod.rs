41abe85ab1270dc97b23e15571f101f02ef00ee5
Thomas Rosenblatt <thomas.rosenblatt@datadoghq.com>
2024-07-09 09:32:24 +0200

    Simplifying nhs checksum tests

--- sds/src/scanner/mod.rs
+++ sds/src/scanner/mod.rs
@@ -1133,7 +1133,7 @@ mod test {
 
     #[test]
     fn test_nhs_checksum() {
-        let pattern = "\\b[0-9]{10}\\b";
+        let pattern = ".+";
         let rule = RegexRuleConfig::builder(pattern.to_string())
             .match_action(MatchAction::Redact {
                 replacement: "[NHS]".to_string(),
@@ -1144,38 +1144,15 @@ mod test {
             .validator(NhsCheckDigit)
             .build();
 
-        // Test matching NHS number without checksum
-        let scanner = ScannerBuilder::new(&[rule.clone()]).build().unwrap();
-        let mut content = "nhs 1234567881".to_string();
-        let mut content_2 = content.clone();
-        let mut content_3 = "nhs 1234567882".to_string();
-        let mut content_4 = content_3.clone();
-
-        let matches = scanner.scan(&mut content);
-        assert_eq!(matches.len(), 1);
-        assert_eq!(content, "nhs [NHS]");
-
+        let mut content = "1234567881".to_string();
         // Test matching NHS number with checksum
         let scanner = ScannerBuilder::new(&[rule_with_checksum.clone()])
             .with_keywords_should_match_event_paths(true)
             .build()
             .unwrap();
-        let matches = scanner.scan(&mut content_2);
+        let matches = scanner.scan(&mut content);
         assert_eq!(matches.len(), 1);
-        assert_eq!(content_2, "nhs [NHS]");
-
-        // Test matching NHS number with invalid checksum and no checksum rule
-        let scanner = ScannerBuilder::new(&[rule.clone()]).build().unwrap();
-        let matches = scanner.scan(&mut content_3);
-        assert_eq!(matches.len(), 1);
-        assert_eq!(content, "nhs [NHS]");
-
-        // Test matching NHS number with invalid checksum and checksum rule
-        let scanner = ScannerBuilder::new(&[rule_with_checksum.clone()])
-            .build()
-            .unwrap();
-        let matches = scanner.scan(&mut content_4);
-        assert_eq!(matches.len(), 0);
+        assert_eq!(content, "[NHS]");
     }
 
     #[test]
