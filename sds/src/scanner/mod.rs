961937f858a190283d3b15d8e8a452ee83bcec76
Thomas Rosenblatt <thomas.rosenblatt@datadoghq.com>
2024-08-09 17:59:20 +0200

    fix

--- sds/src/scanner/mod.rs
+++ sds/src/scanner/mod.rs
@@ -166,6 +166,7 @@ impl Scanner {
     // This function scans the given event with the rules configured in the scanner.
     // The event parameter is a mutable reference to the event that should be scanned (implemented the Event trait).
     // The blocked_rules_idx parameter is a list of rule indices that should be skipped for this scan.
+    // this list shall be small (<10), so a linear search is acceptable otherwise performance will be impacted.
     // The return value is a list of RuleMatch objects, which contain information about the matches that were found.
     pub fn scan<E: Event>(&self, event: &mut E, blocked_rules_idx: Vec<usize>) -> Vec<RuleMatch> {
         // This is a set of caches (1 for each rule) that can be used for scanning. This is obtained once per scan to reduce
