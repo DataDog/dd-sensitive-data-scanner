1e1effbbb65dcbba9294b6f72693c844a0d152e5
Arthur Belleville <arthur.belleville@datadoghq.com>
2024-08-23 12:07:23 +0200

    Cargo fmt

--- sds/src/scanner/mod.rs
+++ sds/src/scanner/mod.rs
@@ -11,9 +11,7 @@ use std::sync::Arc;
 
 use self::cache_pool::{CachePool, CachePoolBuilder, CachePoolGuard};
 use self::metrics::ScannerMetrics;
-use crate::proximity_keywords::{
-    contains_keyword_in_path, CompiledIncludedProximityKeywords,
-};
+use crate::proximity_keywords::{contains_keyword_in_path, CompiledIncludedProximityKeywords};
 use crate::scanner::config::RuleConfig;
 use crate::scanner::regex_rule::compiled::RegexCompiledRule;
 use crate::scanner::scope::Scope;
