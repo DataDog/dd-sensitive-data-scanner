fab05c09f36e48d63653c8a565b9fa78061815e5
Thomas Rosenblatt <thomas.rosenblatt@datadoghq.com>
2024-09-25 14:59:47 +0200

    fix status

--- sds/src/match_validation/match_status.rs
+++ sds/src/match_validation/match_status.rs
@@ -1,10 +1,75 @@
-// Order matters as we want to update the match_status only if the new match_status has higher priority.
-// (in case of split key where we try different combinations of id and secret (aws usecase))
 #[derive(Debug, PartialEq, PartialOrd, Ord, Eq, Clone)]
 pub enum MatchStatus {
     NotChecked,
     NotAvailable,
-    Error(String),
     Invalid,
+    Error(String),
     Valid,
+}
+
+impl MatchStatus {
+    // Order matters as we want to update the match_status only if the new match_status has higher priority.
+    // (in case of split key where we try different combinations of id and secret (aws usecase))
+    pub fn merge(&mut self, new_status: MatchStatus) {
+        if new_status > *self {
+            *self = new_status;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_merge() {
+        let mut status = MatchStatus::NotChecked;
+        status.merge(MatchStatus::NotAvailable);
+        assert_eq!(status, MatchStatus::NotAvailable);
+
+        status.merge(MatchStatus::Invalid);
+        assert_eq!(status, MatchStatus::Invalid);
+
+        status.merge(MatchStatus::Error("error".to_string()));
+        assert_eq!(status, MatchStatus::Error("error".to_string()));
+
+        status.merge(MatchStatus::Valid);
+        assert_eq!(status, MatchStatus::Valid);
+    }
+    fn test_merge_lower_prio() {
+        let mut status = MatchStatus::Valid;
+        status.merge(MatchStatus::NotChecked);
+        assert_eq!(status, MatchStatus::Valid);
+
+        status.merge(MatchStatus::NotAvailable);
+        assert_eq!(status, MatchStatus::Valid);
+
+        status.merge(MatchStatus::Invalid);
+        assert_eq!(status, MatchStatus::Valid);
+
+        status.merge(MatchStatus::Error("error".to_string()));
+        assert_eq!(status, MatchStatus::Valid);
+
+        status = MatchStatus::Error("error".to_string());
+        status.merge(MatchStatus::NotChecked);
+
+        assert_eq!(status, MatchStatus::Error("error".to_string()));
+
+        status.merge(MatchStatus::NotAvailable);
+        assert_eq!(status, MatchStatus::Error("error".to_string()));
+
+        status.merge(MatchStatus::Invalid);
+        assert_eq!(status, MatchStatus::Error("error".to_string()));
+
+        status = MatchStatus::Invalid;
+        status.merge(MatchStatus::NotChecked);
+        assert_eq!(status, MatchStatus::Invalid);
+
+        status.merge(MatchStatus::NotAvailable);
+        assert_eq!(status, MatchStatus::Invalid);
+
+        status = MatchStatus::NotAvailable;
+        status.merge(MatchStatus::NotChecked);
+        assert_eq!(status, MatchStatus::NotAvailable);
+    }
 }
