024b4645cd882fe9ba4333b65f6b7d9fac775f60
Frank Bryden <frank.bryden@datadoghq.com>
2024-10-25 15:29:52 +0200

    Add new errors for rule validation

--- sds/src/scanner/error.rs
+++ sds/src/scanner/error.rs
@@ -13,6 +13,7 @@ impl From<CreateScannerError> for i64 {
             CreateScannerError::InvalidKeywords(_) => -3,
             CreateScannerError::InvalidMatchAction(_) => -4,
             CreateScannerError::InvalidMatchValidator(_) => -5,
+            CreateScannerError::InvalidMetadata(_) => -6,
         }
     }
 }
@@ -37,12 +38,44 @@ pub enum CreateScannerError {
     /// The match validator cannot be created
     #[error(transparent)]
     InvalidMatchValidator(#[from] MatchValidatorCreationError),
+    // The metadata used to create the scanner is invalid
+    #[error(transparent)]
+    InvalidMetadata(#[from] ScannerMetadataError)
 }
 
 #[derive(Debug, PartialEq, Eq, Error)]
 pub enum MatchValidationError {
     #[error("No MatchValidationType provided")]
     NoMatchValidationType,
+}
+
+#[derive(Debug, PartialEq, Eq, Error)]
+pub enum ScannerMetadataError {
+    #[error("ID is too short, must be at least {0} characters long")]
+    IdTooShort,
+    #[error("`name` field must not be trim empty")]
+    NameTrimEmpty,
+    #[error("`name` field is too long, at most {0} characters are allowed, the current name has {1} characters")]
+    NameTooLong,
+    #[error("`description` field must not be trim empty")]
+    DescriptionTrimEmpty,
+    #[error("`description` field is too long, at most {0} characters are allowed, the current description has {1} characters")]
+    DescriptionTooLong,
+    #[error("`description` field is not correctly formatted, description should follow the template:
+              ```
+              [DESCRIPTION OF THE SENSITIVE DATA FORMAT]
+
+              Examples of matching formats: [Example of matching format: if a single example]
+              - `[EXAMPLE_1]`
+              - `[EXAMPLE_2]`
+              ```
+              current description:
+              ```
+              %s
+              ```")]
+    DescriptionFormatInvalid,
+    #[error("`priority` field should between 1 and 5")]
+    PriorityInvalidValue,
 }
 
 #[cfg(test)]
