ea5fd76216cbf4421cae768646ac1e7cac99333d
Frank Bryden <frank.bryden@datadoghq.com>
2024-10-25 18:08:29 +0200

    Make ScannerMetadataError public

--- sds/src/lib.rs
+++ sds/src/lib.rs
@@ -38,7 +38,7 @@ pub use path::{Path, PathSegment};
 pub use rule_match::{ReplacementType, RuleMatch};
 pub use scanner::shared_pool::{SharedPool, SharedPoolGuard};
 
-pub use scanner::error::MatchValidationError;
+pub use scanner::error::{MatchValidationError, ScannerMetadataError};
 pub use scanner::{
     config::RuleConfig,
     error::CreateScannerError,
