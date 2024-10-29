b80fb3dc4a84006fd1862513706c48d14392b1cb
Frank Bryden <frank.bryden@datadoghq.com>
2024-10-29 18:07:30 +0100

    whoups broke the code

--- sds/src/lib.rs
+++ sds/src/lib.rs
@@ -39,7 +39,7 @@ pub use rule_match::{ReplacementType, RuleMatch};
 pub use scanner::shared_pool::{SharedPool, SharedPoolGuard};
 
 pub use proximity_keywords::ProximityKeywordsValidationError;
-pub use scanner::error::{MatchValidationError, ScannerMetadataError};
+pub use scanner::error::MatchValidationError;
 pub use scanner::{
     config::RuleConfig,
     error::CreateScannerError,
