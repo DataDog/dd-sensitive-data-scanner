6e92820d8329dbb69f4a83f8f40aa2f7bc609363
Frank Bryden <frank.bryden@datadoghq.com>
2024-10-29 17:59:23 +0100

    Remove new types in error.rs

--- sds/src/scanner/error.rs
+++ sds/src/scanner/error.rs
@@ -13,7 +13,6 @@ impl From<CreateScannerError> for i64 {
             CreateScannerError::InvalidKeywords(_) => -3,
             CreateScannerError::InvalidMatchAction(_) => -4,
             CreateScannerError::InvalidMatchValidator(_) => -5,
-            CreateScannerError::InvalidMetadata(_) => -6,
         }
     }
 }
@@ -38,64 +37,12 @@ pub enum CreateScannerError {
     /// The match validator cannot be created
     #[error(transparent)]
     InvalidMatchValidator(#[from] MatchValidatorCreationError),
-    // The metadata used to create the scanner is invalid
-    #[error(transparent)]
-    InvalidMetadata(#[from] ScannerMetadataError),
 }
 
 #[derive(Debug, PartialEq, Eq, Error)]
 pub enum MatchValidationError {
     #[error("No MatchValidationType provided")]
     NoMatchValidationType,
-}
-
-#[derive(Debug, PartialEq, Eq, Error)]
-pub enum ScannerMetadataError {
-    //ID
-    #[error("ID is too short, must be at least {0} characters long")]
-    IdTooShort(usize),
-
-    //Name
-    #[error("`name` field must not be trim empty")]
-    NameTrimEmpty,
-    #[error("`name` field is too long, at most {0} characters are allowed, the current name has {1} characters")]
-    NameTooLong(usize, usize),
-    #[error("`description` field must not be trim empty")]
-    //Description
-    DescriptionTrimEmpty,
-    #[error("`description` field is too long, at most {0} characters are allowed, the current description has {1} characters")]
-    DescriptionTooLong(usize, usize),
-    #[error(
-        "`description` field is not correctly formatted, description should follow the template:
-              ```
-              [DESCRIPTION OF THE SENSITIVE DATA FORMAT]
-
-              Examples of matching formats: [Example of matching format: if a single example]
-              - `[EXAMPLE_1]`
-              - `[EXAMPLE_2]`
-              ```
-              current description:
-              ```
-              %s
-              ```"
-    )]
-    DescriptionFormatInvalid,
-    #[error("`description` example {0} does not match the rule's pattern")]
-    DescriptionExampleDoesNotMatch(String),
-
-    //Priority
-    #[error("`priority` field should between 1 and 5")]
-    PriorityInvalidValue,
-
-    //Tags
-    #[error("tag `{0}` in the `tags` field is not valid, all tags should be <key:value>")]
-    InvalidTag(String),
-    #[error("`{0}` tag is required but is missing in the `tags` field")]
-    MissingTag(String),
-
-    //Labels
-    #[error("label `{0}` in the `labels` field is not valid, all labels should be <key:value>")]
-    InvalidLabel(String),
 }
 
 #[cfg(test)]
