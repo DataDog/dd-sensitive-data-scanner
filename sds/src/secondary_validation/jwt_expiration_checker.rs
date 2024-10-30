cd2f00fc21c30f727baac77840b2a39691daec51
Thomas Rosenblatt <thomas.rosenblatt@datadoghq.com>
2024-10-30 14:26:08 +0100

    handle vincents comments

--- sds/src/secondary_validation/jwt_expiration_checker.rs
+++ sds/src/secondary_validation/jwt_expiration_checker.rs
@@ -9,13 +9,17 @@ const SEGMENTS_COUNT: usize = 3;
 
 impl Validator for JwtExpirationChecker {
     fn is_valid_match(&self, regex_match: &str) -> bool {
-        let result = decode_segments(regex_match);
-        if let Ok((_, payload)) = result {
+        if let Some((_, payload)) = decode_segments(regex_match) {
             let expiration = payload.get("exp");
             if let Some(exp) = expiration {
-                let exp_int: i64 = exp.as_i64().unwrap();
-                let now = Utc::now().timestamp();
-                exp_int > now
+                if exp.is_i64() {
+                    let exp_int: i64 = exp.as_i64().unwrap();
+                    let now = Utc::now().timestamp();
+                    exp_int > now
+                } else {
+                    // if the expiration claim is not an integer, we consider it as an invalid match
+                    false
+                }
             } else {
                 // if there is no expiration claim, we consider it as a valid match
                 true
@@ -31,39 +35,36 @@ impl Validator for JwtExpirationChecker {
 // This function is an extraction of the decoding part https://github.com/GildedHonour/frank_jwt
 // We don't want to depend on the whole frank_jwt crate for this simple function
 // Our goal is to be able to decode a JWT without checking the signature
-fn decode_segments(encoded_token: &str) -> Result<(JsonValue, JsonValue), ()> {
+fn decode_segments(encoded_token: &str) -> Option<(JsonValue, JsonValue)> {
     let raw_segments: Vec<&str> = encoded_token.split(".").collect();
     if raw_segments.len() != SEGMENTS_COUNT {
         // Invalid JWT header + payload + signature
-        return Err(());
+        return None;
     }
 
     let header_segment = raw_segments[0];
     let payload_segment = raw_segments[1];
-    let result = decode_header_and_payload(header_segment, payload_segment);
-    if let Ok((header, payload)) = result {
-        Ok((header, payload))
-    } else {
-        Err(())
-    }
+    decode_header_and_payload(header_segment, payload_segment)
+}
+
+fn decode_segment(segment: &str) -> Option<JsonValue> {
+    URL_SAFE_NO_PAD
+        .decode(segment)
+        .ok()
+        .and_then(|decoded| serde_json::from_slice(&decoded).ok())
 }
 
 fn decode_header_and_payload(
     header_segment: &str,
     payload_segment: &str,
-) -> Result<(JsonValue, JsonValue), ()> {
-    let b64_to_json = |seg| -> Result<JsonValue, ()> {
-        let res = URL_SAFE_NO_PAD.decode(seg);
-        if let Ok(decoded) = res {
-            serde_json::from_slice(&decoded).map_err(|_| ())
-        } else {
-            Err(())
-        }
-    };
-
-    let header_json = b64_to_json(header_segment)?;
-    let payload_json = b64_to_json(payload_segment)?;
-    Ok((header_json, payload_json))
+) -> Option<(JsonValue, JsonValue)> {
+    let header_json = decode_segment(header_segment);
+    let payload_json = decode_segment(payload_segment);
+    if let (Some(header), Some(payload)) = (header_json, payload_json) {
+        Some((header, payload))
+    } else {
+        None
+    }
 }
 
 #[cfg(test)]
@@ -85,7 +86,7 @@ mod tests {
     fn test_decode_segments() {
         // {"alg":"HS256","typ":"JWT"}"."{"exp":1728872000}".signature
         let result = decode_segments("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3Mjg4NzIwMDB9.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c");
-        assert_eq!(result.is_ok(), true);
+        assert!(result.is_some());
         let (header, payload) = result.unwrap();
         assert_eq!(
             header.get("alg"),
@@ -102,7 +103,7 @@ mod tests {
         assert!(checker.is_valid_match(&json));
     }
     #[test]
-    fn test_is_valid_match_expired_jwt() {
+    fn test_is_invalid_match_for_expired_jwt() {
         let past_time_as_string = (Utc::now().timestamp() - 1000000).to_string();
         let json = generate_jwt(past_time_as_string);
         let checker = JwtExpirationChecker;
@@ -122,7 +123,15 @@ mod tests {
     }
 
     #[test]
-    fn test_is_valid_match_invalid_jwt() {
+    fn test_is_invalid_match_for_invalid_exp() {
+        let past_time_as_string = "\"hello\"".to_string();
+        let json = generate_jwt(past_time_as_string);
+        let checker = JwtExpirationChecker;
+        assert!(!checker.is_valid_match(&json));
+    }
+
+    #[test]
+    fn test_is_invalid_match_for_invalid_jwt() {
         let json = "invalid_jwt";
         let checker = JwtExpirationChecker;
         assert!(!checker.is_valid_match(&json));
