f2a1847932a4f88ffa72d3ad369db909e12ce7d4
Frank Bryden <frank.bryden@datadoghq.com>
2024-11-15 15:34:28 +0100

    Discard changes in multithread_scanning.rs

--- sds/benches/multithreaded_scanning.rs
+++ sds/benches/multithreaded_scanning.rs
@@ -36,10 +36,10 @@ pub fn multithread_scanning(c: &mut Criterion) {
     let sample_inputs = sample_inputs();
     let sample_event = sample_large_event();
 
-    let num_threads = 16;
+    let num_threads = 32;
     // There are more jobs than threads to test things that may take longer the first time
     // a thread does something (e.g. thread local storage)
-    let num_jobs = num_threads * 25;
+    let num_jobs = 256;
     let thread_pool = ThreadPool::new(num_threads);
 
     c.bench_function("scan single strings (multi-threaded)", |b| {
