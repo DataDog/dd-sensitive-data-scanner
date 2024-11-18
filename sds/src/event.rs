fb54f5895b3bd8a39ba2516368c3397cbb4441dc
Arthur Belleville <arthur.belleville@datadoghq.com>
2024-11-18 11:43:30 +0100

    Add implementation for Hashmap

--- sds/src/event.rs
+++ sds/src/event.rs
@@ -1,3 +1,4 @@
+use core::panic;
 use std::collections::HashMap;
 use std::hash::RandomState;
 
@@ -46,16 +47,90 @@ impl Event for String {
     }
 }
 
-impl<K, V> Event for HashMap<K, V, RandomState> {
+impl Event for serde_json::Value {
     type Encoding = Utf8Encoding;
 
-    fn visit_event<'a>(&'a mut self, visitor: &mut impl EventVisitor<'a>) {}
+    fn visit_event<'a>(&'a mut self, visitor: &mut impl EventVisitor<'a>) {
+        match self {
+            serde_json::Value::Null => {}
+            serde_json::Value::Bool(value) => {
+                let _result = visitor.visit_string(value.to_string().as_str());
+            }
+            serde_json::Value::Number(number) => {
+                let _result = visitor.visit_string(number.to_string().as_str());
+            }
+            serde_json::Value::String(s) => {
+                let _result = visitor.visit_string(&s);
+            }
+            serde_json::Value::Object(map) => {
+                for (k, child) in map.iter_mut() {
+                    visitor.push_segment(k.as_str().into());
+                    child.visit_event(visitor);
+                    visitor.pop_segment();
+                }
+            }
+            serde_json::Value::Array(values) => {
+                for (i, value) in values.into_iter().enumerate() {
+                    visitor.push_segment(PathSegment::Index(i));
+                    value.visit_event(visitor);
+                    visitor.pop_segment();
+                }
+            }
+        }
+    }
 
-    fn visit_string_mut(&mut self, path: &Path, visit: impl FnMut(&mut String) -> bool) {}
+    fn visit_string_mut(&mut self, path: &Path, mut visit: impl FnMut(&mut String) -> bool) {
+        let mut value = self;
+        for segment in &path.segments {
+            match segment {
+                PathSegment::Field(key) => {
+                    value = value
+                        .as_object_mut()
+                        .unwrap()
+                        .get_mut(key.as_ref())
+                        .unwrap();
+                }
+                PathSegment::Index(i) => {
+                    value = value.as_array_mut().unwrap().get_mut(*i).unwrap();
+                }
+            }
+        }
+        match value {
+            serde_json::Value::String(s) => {
+                (visit)(s);
+            }
+            _ => panic!("unknown value"),
+        };
+    }
+}
+
+impl Event for HashMap<String, serde_json::Value, RandomState> {
+    type Encoding = Utf8Encoding;
+
+    fn visit_event<'a>(&'a mut self, visitor: &mut impl EventVisitor<'a>) {
+        for (k, v) in self.iter_mut() {
+            visitor.push_segment(PathSegment::Field(k.as_str().into()));
+            v.visit_event(visitor);
+            visitor.pop_segment();
+        }
+    }
+
+    fn visit_string_mut(&mut self, path: &Path, mut visit: impl FnMut(&mut String) -> bool) {
+        let first_segment = path.segments.first().unwrap();
+        match first_segment {
+            PathSegment::Field(field) => {
+                let value = self.get_mut(&field.to_string()).unwrap();
+                value.visit_string_mut(path, &mut visit);
+            }
+            _ => {}
+        }
+    }
 }
 
 #[cfg(test)]
 pub(crate) mod test {
+
+    use serde_json::{Map, Value};
 
     use crate::simple_event::SimpleEvent;
 
@@ -137,6 +212,39 @@ pub(crate) mod test {
                 VisitOp::Visit("value-a".into()),
                 VisitOp::Pop,
                 VisitOp::Push(PathSegment::Field("key-b".into())),
+                VisitOp::Push(PathSegment::Field("key-b-1".into())),
+                VisitOp::Visit("value-b-1".into()),
+                VisitOp::Pop,
+                VisitOp::Pop,
+            ]
+        );
+    }
+    #[test]
+    pub fn test_hashmap_event() {
+        let mut map = Map::new();
+        map.insert(
+            "key-a-1".to_string(),
+            Value::String("value-a-1".to_string()),
+        );
+        map.insert(
+            "key-b-1".to_string(),
+            Value::String("value-b-1".to_string()),
+        );
+        let mut event = HashMap::from([("key-a".to_string(), Value::Object(map))]);
+
+        let mut visitor = Visitor {
+            ops: vec![],
+            path: Path::root(),
+        };
+        event.visit_event(&mut visitor);
+
+        assert_eq!(
+            visitor.ops,
+            vec![
+                VisitOp::Push(PathSegment::Field("key-a".into())),
+                VisitOp::Push(PathSegment::Field("key-a-1".into())),
+                VisitOp::Visit("value-a-1".into()),
+                VisitOp::Pop,
                 VisitOp::Push(PathSegment::Field("key-b-1".into())),
                 VisitOp::Visit("value-b-1".into()),
                 VisitOp::Pop,
