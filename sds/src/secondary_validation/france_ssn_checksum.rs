e06b3fb6b7563253a976b6e82b525e85c7c74b28
Isabella Garza <isabella.garza@datadoghq.com>
2025-04-30 15:49:15 -0400

    Reformat France SSN validator logic

--- sds/src/secondary_validation/france_ssn_checksum.rs
+++ sds/src/secondary_validation/france_ssn_checksum.rs
@@ -2,55 +2,58 @@ use crate::secondary_validation::Validator;
 
 pub struct FranceSsnChecksum;
 
-const FRANCE_SSN_LENGTH: u8 = 15;
-const FRANCE_SSN_CONTROL_KEY_LENGTH: u8 = 2;
-const FRANCE_SSN_FIRST_VALUE_LENGTH: u8 = FRANCE_SSN_LENGTH - FRANCE_SSN_CONTROL_KEY_LENGTH;
+const FRANCE_SSN_LENGTH: usize = 15;
+const FRANCE_SSN_CONTROL_KEY_LENGTH: usize = 2;
+const FRANCE_SSN_FIRST_VALUE_LENGTH: usize = FRANCE_SSN_LENGTH - FRANCE_SSN_CONTROL_KEY_LENGTH;
+
+const FRANCE_SSN_CHECKSUM_MODULUS: i64 = 97;
 
 impl Validator for FranceSsnChecksum {
     // https://en.wikipedia.org/wiki/INSEE_code
     fn is_valid_match(&self, regex_match: &str) -> bool {
-        let mut first_value: i64 = 0;
-        let mut control_key: i64 = 0;
-        let mut count: u8 = 0;
-        let mut penalties: i64 = 0;
+        // Remove possible whitespace or '-' from SSN
+        let mut valid_chars = regex_match
+            .chars()
+            .filter(|c| c.is_ascii_digit() || matches!(c, 'a' | 'A' | 'b' | 'B'));
 
-        for c in regex_match.chars() {
-            if c.is_ascii_digit() || matches!(c, 'a' | 'A' | 'b' | 'B') {
-                // Calculate the value of the first 13 alphanumeric characters
-                if count < FRANCE_SSN_FIRST_VALUE_LENGTH {
-                    count += 1;
-                    if let Some(digit) = c.to_digit(10) {
-                        first_value = first_value * 10 + digit as i64;
-                    } else {
-                        // Some department codes contain an 'A' or 'B'.
-                        // In that case, replace 'A' or 'B' with '0' and, based on if it was the letter 'A' or 'B',
-                        // subtract 1000000 or 2000000, respectively.
-                        // ref: https://xml.insee.fr/schema/nir.html
-                        first_value *= 10; // push a '0'
-                        penalties += 1000000;
-                        if matches!(c, 'b' | 'B') {
-                            penalties += 1000000;
-                        }
-                    }
-                // Calculate the control key from the remaining numerical characters
-                } else if count < FRANCE_SSN_LENGTH {
-                    if let Some(digit) = c.to_digit(10) {
-                        count += 1;
-                        control_key = control_key * 10 + digit as i64;
-                    }
-                // Incorrect length for French SSN
-                } else {
-                    return false;
-                }
-            }
-        }
-        if count != FRANCE_SSN_LENGTH {
+        if valid_chars.clone().count() != FRANCE_SSN_LENGTH {
             return false;
         }
 
+        // Calculate the value of the first 13 alphanumeric characters
+        let mut first_value: i64 = 0;
+        let mut penalties: i64 = 0;
+        for c in valid_chars.by_ref().take(FRANCE_SSN_FIRST_VALUE_LENGTH) {
+            if let Some(digit) = c.to_digit(10) {
+                first_value = first_value * 10 + digit as i64;
+            } else {
+                // Some department codes contain an 'A' or 'B'.
+                // In that case, replace 'A' or 'B' with '0' and, based on if it was
+                // the letter 'A' or 'B', subtract 1000000 or 2000000, respectively.
+                // ref: https://xml.insee.fr/schema/nir.html
+                first_value *= 10; // push a '0'
+                penalties += 1000000;
+                if matches!(c, 'b' | 'B') {
+                    penalties += 1000000;
+                }
+            }
+        }
+
+        // Calculate the control key from the remaining numerical characters
+        let mut control_key: i64 = 0;
+        for c in valid_chars.by_ref().take(FRANCE_SSN_CONTROL_KEY_LENGTH) {
+            // Control key cannot contain alphabetic characters
+            if let Some(digit) = c.to_digit(10) {
+                control_key = control_key * 10 + digit as i64;
+            } else {
+                return false;
+            }
+        }
+
+        // Calculate checksum
         let final_value = first_value - penalties;
 
-        (97 - final_value % 97) == control_key
+        (FRANCE_SSN_CHECKSUM_MODULUS - final_value % FRANCE_SSN_CHECKSUM_MODULUS) == control_key
     }
 }
 
