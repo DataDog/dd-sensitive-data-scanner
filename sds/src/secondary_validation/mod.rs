87e8f14f6dd04c28a30df38321f335bc7d083cb2
Vincent Roy <vincent.roy@datadoghq.com>
2025-05-22 17:36:46 +0200

    Add French NIF checksum

--- sds/src/secondary_validation/mod.rs
+++ sds/src/secondary_validation/mod.rs
@@ -2,6 +2,7 @@ mod aba_rtn_checksum;
 mod brazilian_cnpj_checksum;
 mod brazilian_cpf_checksum;
 mod chinese_id_checksum;
+mod france_nif_checksum;
 mod france_ssn_checksum;
 mod github_token_checksum;
 mod iban_checker;
@@ -21,6 +22,7 @@ pub use crate::secondary_validation::aba_rtn_checksum::AbaRtnChecksum;
 pub use crate::secondary_validation::brazilian_cnpj_checksum::BrazilianCnpjChecksum;
 pub use crate::secondary_validation::brazilian_cpf_checksum::BrazilianCpfChecksum;
 pub use crate::secondary_validation::chinese_id_checksum::ChineseIdChecksum;
+use crate::secondary_validation::france_nif_checksum::FranceNifChecksum;
 pub use crate::secondary_validation::france_ssn_checksum::FranceSsnChecksum;
 pub use crate::secondary_validation::github_token_checksum::GithubTokenChecksum;
 pub use crate::secondary_validation::iban_checker::IbanChecker;
@@ -82,6 +84,7 @@ impl Validator for SecondaryValidator {
                 LuxembourgIndividualNINChecksum.is_valid_match(regex_match)
             }
             SecondaryValidator::FranceSsnChecksum => FranceSsnChecksum.is_valid_match(regex_match),
+            SecondaryValidator::FranceNifChecksum => FranceNifChecksum.is_valid_match(regex_match),
         }
     }
 }
