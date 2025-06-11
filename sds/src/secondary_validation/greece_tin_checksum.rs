3cc2f441a18aeb036ddac7b2cb90885ba79f6e91
Vincent Roy <vincent.roy@datadoghq.com>
2025-06-11 12:33:08 +0200

    apply suggestion

--- sds/src/secondary_validation/greece_tin_checksum.rs
+++ sds/src/secondary_validation/greece_tin_checksum.rs
@@ -6,29 +6,17 @@ const GREEK_TIN_LENGTH: usize = 9;
 
 impl Validator for GreekTinChecksum {
     fn is_valid_match(&self, regex_match: &str) -> bool {
-        if regex_match.len() != GREEK_TIN_LENGTH {
-            return false;
-        }
+        let mut digits = regex_match.chars().filter_map(|c| c.to_digit(10));
 
-        // Check if the string contains only allowed characters
-        if !regex_match.chars().all(|c| c.is_ascii_digit()) {
-            return false;
-        }
-
-        // Split into front digits and check digit
-        let front = &regex_match[..regex_match.len() - 1];
-        let check = regex_match.chars().last().unwrap().to_digit(10).unwrap();
-
-        // Calculate sum by iterating through front digits
-        let sum = front
-            .chars()
-            .map(|c| c.to_digit(10).unwrap())
+        let sum = digits
+            .by_ref()
+            .take(GREEK_TIN_LENGTH - 1)
             .fold(0, |acc, digit| acc * 2 + digit);
 
-        // Calculate expected check digit
-        let expected_check = ((sum * 2) % 11) % 10;
-
-        expected_check == check
+        if let Some(actual_checksum) = digits.next() {
+            return ((sum * 2) % 11) % 10 == actual_checksum;
+        }
+        false
     }
 }
 
@@ -39,10 +27,12 @@ mod test {
     #[test]
     fn test_valid_greek_tin() {
         let validator = GreekTinChecksum;
-
         assert!(validator.is_valid_match("982603151"));
         assert!(validator.is_valid_match("833329082"));
         assert!(validator.is_valid_match("094259216"));
+        assert!(validator.is_valid_match("982-603-151"));
+        assert!(validator.is_valid_match("982.603.151"));
+        assert!(validator.is_valid_match("982🙏603151"));
     }
 
     #[test]
@@ -52,5 +42,7 @@ mod test {
         assert!(!validator.is_valid_match("982603152"));
         assert!(!validator.is_valid_match("833329083"));
         assert!(!validator.is_valid_match("094259217"));
+        // too short
+        assert!(!validator.is_valid_match("9826031"));
     }
 }
