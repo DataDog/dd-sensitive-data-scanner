a53a97c341bd699e44fa1bbe04e5b15123573c9c
Vincent Roy <vincent.roy@datadoghq.com>
2025-06-12 15:58:16 +0200

    apply suggestions

--- sds/src/secondary_validation/latvia_national_id_checksum.rs
+++ sds/src/secondary_validation/latvia_national_id_checksum.rs
@@ -1,50 +1,34 @@
-use crate::secondary_validation::{get_next_digit, Validator};
+use crate::secondary_validation::Validator;
 
 pub struct LatviaNationalIdChecksum;
 
-const LATVIA_NATIONAL_ID_LENGTH: usize = 11;
-const LATVIA_NATIONAL_ID_OLD_FORMAT_MULTIPLIERS: &[u32] = &[1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
+const LATVIA_NATIONAL_ID_OLD_FORMAT_MULTIPLIERS: &[u32; 10] = &[1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
 
 impl Validator for LatviaNationalIdChecksum {
+    // https://en.wikipedia.org/wiki/National_identification_number
     fn is_valid_match(&self, regex_match: &str) -> bool {
-        /*
-         * Latvia national identification number has 2 formats:
-         * 1. Since July 1st 2017, an 11-digit number, starting with "32" with possible '-' after the 6th digit
-         * 2. Before July 1st 2017, an 11 digits number, in the format DDMMYY-XNNNZ, where:
-         *    - DDMMYY is the date of birth
-         *    - X represents the century digit (0 for 19, 1 for 20, 2 for 21)
-         *    - NNN is a birth serial number in that day
-         *    - Z is the checksum digit
-         */
-
-        // Remove possible '-'
-        let digits_str: String = regex_match.chars().filter(|c| c.is_ascii_digit()).collect();
-        if digits_str.len() != LATVIA_NATIONAL_ID_LENGTH {
-            return false;
-        }
-
-        let mut digits = digits_str.chars();
-        let first_2_digits: String = digits.clone().take(2).collect();
+        let mut digits = regex_match.chars().filter_map(|c| c.to_digit(10));
+        let first_2_digits: Vec<u32> = digits.clone().take(2).collect();
         // No checksum validation needed for the new format
-        if first_2_digits == "32" {
+        if first_2_digits == vec![3, 2] {
             return true;
         }
 
-        // Checksum validation for the old format ABCDEF-XGHIZ
-        // Z must equal to (1101-(1*A + 6*B + 3*C + 7*D + 9*E + 10*F + 5*X + 8*G + 4*H + 2*I)) | Mod 11 | Mod 10.
         let mut sum = 0;
-        for mult in LATVIA_NATIONAL_ID_OLD_FORMAT_MULTIPLIERS {
-            let digit = match get_next_digit(&mut digits) {
-                Some(d) => d,
-                None => return false,
-            };
-            sum += digit * mult;
+        for (index, digit) in digits
+            .by_ref()
+            .take(LATVIA_NATIONAL_ID_OLD_FORMAT_MULTIPLIERS.len())
+            .enumerate()
+        {
+            let multiplier = LATVIA_NATIONAL_ID_OLD_FORMAT_MULTIPLIERS[index];
+            sum += digit * multiplier;
         }
 
-        let checksum = ((1101 - sum) % 11) % 10;
-        let actual_checksum = digits.last().unwrap().to_digit(10).unwrap();
-
-        checksum == actual_checksum
+        if let Some(actual_checksum) = digits.next() {
+            let computed_checksum = ((1101 - sum) % 11) % 10;
+            return actual_checksum == computed_checksum;
+        }
+        false
     }
 }
 
