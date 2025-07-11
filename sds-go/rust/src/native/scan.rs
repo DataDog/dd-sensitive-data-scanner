712c5eba50da8ff6387edc9fbfd05d53f745fdc4
Frank Bryden <frank.bryden@datadoghq.com>
2025-07-11 12:10:17 +0200

    Fix sds-go invocation

--- sds-go/rust/src/native/scan.rs
+++ sds-go/rust/src/native/scan.rs
@@ -26,9 +26,9 @@ pub extern "C" fn scan(
         let mut event = BinaryEvent::<Utf8Encoding>::new(data, false);
 
         // TODO: we might want to forward the error to go in the future
-        let matches = scanner.scan(&mut event).unwrap_or_default();
+        let matches = scanner.scan(&mut event);
 
-        if let Some(encoded_response) = encode_response(&event.storage, &matches, false) {
+        if let Some(encoded_response) = encode_response(&event.storage, matches, false) {
             let mut str = std::mem::ManuallyDrop::new(encoded_response);
             let len = str.len() as i64;
             let cap = str.capacity() as i64;
