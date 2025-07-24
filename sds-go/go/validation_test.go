a072629c88d61548d59ba34af9465b2b8776a797
Frank Bryden <frank.bryden@datadoghq.com>
2025-07-24 14:33:16 +0200

    Remove unnecessary prints

--- sds-go/go/validation_test.go
+++ sds-go/go/validation_test.go
@@ -1,22 +1,19 @@
 package dd_sds
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestValidateRegex(t *testing.T) {
 	// Test valid regex
-	valid, err := ValidateRegex("hello")
+	_, err := ValidateRegex("hello")
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println("valid:", valid)
 
 	// Test invalid regex to ensure memory is properly freed
-	valid, err = ValidateRegex("[")
+	_, err = ValidateRegex("[")
 	if err == nil {
 		t.Fatal("Expected error for invalid regex")
 	}
-	fmt.Println("invalid regex error:", err)
 }
