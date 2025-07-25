45ed08031c6143209b1e0da9a332cbacf1f29234
Frank Bryden <frank.bryden@datadoghq.com>
2025-07-25 14:08:51 +0200

    Fix formatting issues

--- scripts/rust_checks.sh
+++ scripts/rust_checks.sh
@@ -1,5 +1,9 @@
 cargo fmt --check --manifest-path="sds/Cargo.toml" --all
 cargo check --manifest-path="sds/Cargo.toml" --benches --workspace
 cargo clippy --manifest-path="sds/Cargo.toml" --workspace -- -D warnings
-echo "Run 'dd-rust-license-tool write' to update a 3rd party license file"
-(cd sds; dd-rust-license-tool check)
+if ./scripts/generate_license_3rdparty.sh check; then
+    echo "License check passed"
+else
+    echo "Run 'make update-licenses' to update the 3rd party license file"
+    exit 1
+fi
