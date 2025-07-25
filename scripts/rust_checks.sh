caed1e6d843cfc5cf8c9f98861925ede01a85d9f
Frank Bryden <frank.bryden@datadoghq.com>
2025-07-25 18:11:42 +0200

    Don't check license here

--- scripts/rust_checks.sh
+++ scripts/rust_checks.sh
@@ -3,9 +3,3 @@ for manifest_path in sds/Cargo.toml sds-go/rust/Cargo.toml; do
     cargo check --manifest-path="$manifest_path" --benches --workspace
     cargo clippy --manifest-path="$manifest_path" --workspace -- -D warnings
 done
-if ./scripts/generate_license_3rdparty.sh check; then
-    echo "License check passed"
-else
-    echo "Run 'make update-licenses' to update the 3rd party license file"
-    exit 1
-fi
