91c074a93d13370e29fec38569485183ed6397d8
Arthur Belleville <arthur.belleville@datadoghq.com>
2025-08-04 17:35:31 +0200

    Add encoded_labels argument to create_scanner

--- sds-go/rust/src/native/create_scanner.rs
+++ sds-go/rust/src/native/create_scanner.rs
@@ -2,18 +2,24 @@ use std::ffi::c_char;
 use std::mem::ManuallyDrop;
 use std::sync::Arc;
 
-use crate::{handle_panic_ptr_return, RuleList};
+use crate::{handle_panic_ptr_return, read_json, RuleList};
 use dd_sds::Scanner;
 
 #[no_mangle]
-pub extern "C" fn create_scanner(rules: i64, error_out: *mut *const c_char) -> i64 {
+pub extern "C" fn create_scanner(
+    rules: i64,
+    encoded_labels: *const c_char,
+    error_out: *mut *const c_char,
+) -> i64 {
     handle_panic_ptr_return(Some(error_out), || {
         let rules_mutex =
             ManuallyDrop::new(unsafe { RuleList::from_raw(rules as usize as *const _) });
         let rules = rules_mutex.lock().unwrap();
 
+        let labels = unsafe { read_json(encoded_labels).unwrap() };
+
         // create the scanner
-        let scanner = match Scanner::builder(&rules).build() {
+        let scanner = match Scanner::builder(&rules).labels(labels).build() {
             Ok(s) => s,
             Err(err) => return err.into(),
         };
