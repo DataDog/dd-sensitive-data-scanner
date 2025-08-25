415c59016ec4b44671b31db7618f154cd7c6e701
Frank Bryden <frank.bryden@datadoghq.com>
2025-08-25 10:48:57 +0200

    Actually check if formatting made changes to source

--- scripts/rust_checks.sh
+++ scripts/rust_checks.sh
@@ -1,5 +1,6 @@
 for manifest_path in sds/Cargo.toml sds-go/rust/Cargo.toml; do
     cargo fmt --check --manifest-path="$manifest_path" --all
+    git diff --exit-code
     cargo check --manifest-path="$manifest_path" --benches --workspace
     cargo clippy --manifest-path="$manifest_path" --workspace -- -D warnings
 done
