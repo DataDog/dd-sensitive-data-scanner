733d1b2ed788649c4e229ed4b17b883789c509df
Frank Bryden <frank.bryden@datadoghq.com>
2025-08-27 17:28:33 +0200

    Clean up and refactor slightly should_match_be_suppressed

--- sds/src/scanner/suppression.rs
+++ sds/src/scanner/suppression.rs
@@ -21,17 +21,17 @@ pub struct CompiledSuppressionConfig {
 
 impl CompiledSuppressionConfig {
     pub fn should_match_be_suppressed(&self, match_content: &str) -> bool {
-        if self.starts_with.is_empty() && self.ends_with.is_empty() && self.exact_match.is_empty() {
-            return false;
-        }
-        self.starts_with
+        let exact_match = self.exact_match.contains(match_content);
+        let starts_with = self
+            .starts_with
             .iter()
-            .any(|start| match_content.starts_with(start))
-            || self
-                .ends_with
-                .iter()
-                .any(|end| match_content.ends_with(end))
-            || self.exact_match.iter().any(|exact| match_content == exact)
+            .any(|start| match_content.starts_with(start));
+        let ends_with = self
+            .ends_with
+            .iter()
+            .any(|end| match_content.ends_with(end));
+
+        exact_match || starts_with || ends_with
     }
 }
 
