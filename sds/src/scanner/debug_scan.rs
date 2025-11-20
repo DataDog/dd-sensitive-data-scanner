d5324e5ee7bd1b2a76efcefb9a746d126f48550a
Nathan Fox <fuchsnj@gmail.com>
2025-11-20 10:16:28 +0100

    add suppressions

--- sds/src/scanner/debug_scan.rs
+++ sds/src/scanner/debug_scan.rs
@@ -1,4 +1,4 @@
-use crate::{Event, RootRuleConfig, RuleConfig, RuleMatch, Scanner, ScannerError};
+use crate::{Event, MatchAction, RegexRuleConfig, RootRuleConfig, RuleConfig, RuleMatch, Scanner, ScannerError};
 use std::sync::Arc;
 use serde::{Deserialize, Serialize};
 
@@ -8,7 +8,7 @@ pub struct DebugRuleMatch {
     pub status: DebugRuleMatchStatus,
 }
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Copy, Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub enum DebugRuleMatchStatus {
     Matched,
     MissingIncludedKeyword,
@@ -21,12 +21,16 @@ pub enum DebugRuleMatchStatus {
 
 pub fn debug_scan<E: Event>(
     event: &mut E,
-    rule: RootRuleConfig<Arc<dyn RuleConfig>>,
+    mut rule: RootRuleConfig<Arc<dyn RuleConfig>>,
 ) -> Result<Vec<DebugRuleMatch>, ScannerError> {
     // Currently only works with scanners containing a single regex rule and nothing else.
 
+    // prevent the output from changing
+    rule.match_action = MatchAction::None;
+
     let full_scanner = Scanner::builder(&[rule.clone().map_inner(|x| x as Arc<dyn RuleConfig>)])
         .build()
+        // TODO: Handle errors better
         .unwrap();
 
     let full_matches = full_scanner.scan(event)?;
@@ -40,33 +44,10 @@ pub fn debug_scan<E: Event>(
         .collect();
 
     if let Some(regex_rule) = rule.inner.as_regex_rule() {
-        let mut regex_rule = regex_rule.clone();
-
-        if let Some(proximity_keywords) = &mut regex_rule.proximity_keywords
-            && !proximity_keywords.included_keywords.is_empty()
-        {
-            proximity_keywords.included_keywords = vec![];
-
-            let scanner = Scanner::builder(&[rule.clone().map_inner(|_| regex_rule.build())])
-                .build()
-                .unwrap();
-
-            let matches = scanner.scan(event)?;
-            for m in matches {
-                if !output.iter().any(|x| x.rule_match == m) {
-                    output.push(DebugRuleMatch {
-                        rule_match: m,
-                        status: DebugRuleMatchStatus::MissingIncludedKeyword,
-                    });
-                }
-            }
-        }
+        debug_scan_regex(event, &rule, regex_rule, &mut output)?;
     }
 
     Ok(output)
-
-    // scan without included keywords
-    // MissingIncludedKeyword,
 
     // custom function
     // IncludedKeywordTooFar,
@@ -82,14 +63,92 @@ pub fn debug_scan<E: Event>(
 
     // scan without checksum
     // ChecksumFailed,
+}
 
-    // unimplemented!()
+fn debug_scan_regex<E: Event>(
+    event: &mut E,
+    root_rule: &RootRuleConfig<Arc<dyn RuleConfig>>,
+    regex_rule: &RegexRuleConfig,
+    output: &mut Vec<DebugRuleMatch>
+) -> Result<(), ScannerError> {
+    debug_scan_included_keywords(event, root_rule, regex_rule, output)?;
+    debug_scan_suppressions(event, root_rule, output)?;
+    Ok(())
+}
+
+fn debug_scan_included_keywords<E: Event>(
+    event: &mut E,
+    root_rule: &RootRuleConfig<Arc<dyn RuleConfig>>,
+    regex_rule: &RegexRuleConfig,
+    output: &mut Vec<DebugRuleMatch>
+) -> Result<(), ScannerError> {
+    let mut regex_rule = regex_rule.clone();
+
+    if let Some(proximity_keywords) = &mut regex_rule.proximity_keywords
+        && !proximity_keywords.included_keywords.is_empty()
+    {
+        proximity_keywords.included_keywords = vec![];
+
+        let scanner = Scanner::builder(&[root_rule.clone().map_inner(|_| regex_rule.build())])
+            .build()
+            .unwrap();
+
+        let matches = scanner.scan(event)?;
+        for m in matches {
+            if !output.iter().any(|x| x.rule_match == m) {
+                output.push(DebugRuleMatch {
+                    rule_match: m,
+                    status: DebugRuleMatchStatus::MissingIncludedKeyword,
+                });
+            }
+        }
+    }
+    Ok(())
+}
+
+fn debug_scan_suppressions<E: Event>(
+    event: &mut E,
+    root_rule: &RootRuleConfig<Arc<dyn RuleConfig>>,
+    output: &mut Vec<DebugRuleMatch>
+) -> Result<(), ScannerError> {
+    if root_rule.suppressions.is_none() {
+        return Ok(());
+    }
+
+    let mut root_rule = root_rule.clone();
+    root_rule.suppressions = None;
+
+
+    let scanner = Scanner::builder(&[root_rule])
+        .build()
+        .unwrap();
+
+    let new_matches = scanner.scan(event)?;
+    add_status_if_no_match(new_matches, output, DebugRuleMatchStatus::Suppressed);
+
+    Ok(())
+}
+
+
+fn add_status_if_no_match(
+    new_matches: Vec<RuleMatch>,
+    output: &mut Vec<DebugRuleMatch>,
+    status: DebugRuleMatchStatus
+) {
+    for m in new_matches {
+        if !output.iter().any(|x| x.rule_match == m) {
+            output.push(DebugRuleMatch {
+                rule_match: m,
+                status
+            });
+        }
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::{MatchAction, RegexRuleConfig, RootRuleConfig};
+    use crate::{MatchAction, RegexRuleConfig, RootRuleConfig, Suppressions};
 
     #[test]
     fn test_full_match() {
@@ -116,11 +175,33 @@ mod test {
         let mut msg = "This is a secret".to_string();
         let matches = debug_scan(&mut msg, rule).unwrap();
 
-        // Full match
         assert_eq!(matches.len(), 1);
         assert_eq!(
             matches[0].status,
             DebugRuleMatchStatus::MissingIncludedKeyword
+        );
+        assert_eq!(matches[0].rule_match.start_index, 10);
+    }
+
+    #[test]
+    fn test_suppressions() {
+        let rule = RootRuleConfig::new(
+            RegexRuleConfig::new("secret").build(),
+        )
+            .match_action(MatchAction::redact("[REDACTED]"))
+            .suppressions(Suppressions {
+                starts_with: vec![],
+                ends_with: vec![],
+                exact_match: vec!["secret".to_string()],
+            });
+
+        let mut msg = "This is a secret".to_string();
+        let matches = debug_scan(&mut msg, rule).unwrap();
+
+        assert_eq!(matches.len(), 1);
+        assert_eq!(
+            matches[0].status,
+            DebugRuleMatchStatus::Suppressed
         );
         assert_eq!(matches[0].rule_match.start_index, 10);
     }
